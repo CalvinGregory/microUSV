e62709f87e8d0a683a7a532033b7053ce52e2e7c
CalvinGregory <cgregory@mun.ca>
2019-05-10 12:59:00 -0230

    Added Socket code. Added protocol buffer.
    Receives a request message with robot's apriltag ID. Responds with pose
    data in protobuf message.

--- CVSensorSimulator/src/CVSensorSimulator.cpp
+++ CVSensorSimulator/src/CVSensorSimulator.cpp
@@ -7,13 +7,17 @@
 //============================================================================
 
 #include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
 #include "pthread.h"
 #include "opencv2/opencv.hpp"
+#include <google/protobuf/util/time_util.h>
 
 #include "FrameBuffer.h"
 #include "PoseDetector.h"
 #include "Robot.h"
 #include "ConfigParser.h"
+#include "musv_msg.pb.h"
 
 extern "C" {
 #include "apriltag/apriltag.h"
@@ -23,11 +27,14 @@ extern "C" {
 
 using namespace std;
 using namespace cv;
+using google::protobuf::util::TimeUtil;
+
+#define PORT 8080
 
 bool running;
 int visualize = 1;
 
-void *vid_cap_thread(void* args) {
+void* vid_cap_thread(void* args) {
 	FrameBuffer* fb = (FrameBuffer*)args;
 	while(running) {
 		fb->updateFrame();
@@ -66,6 +73,7 @@ int main(int argc, char* argv[]) {
 	}
 	else {
 		cerr << "No config file path provided." << endl;
+		exit(-1);
 	}
 
 	apriltag_detection_info_t info;
@@ -109,10 +117,81 @@ int main(int argc, char* argv[]) {
 		exit(-1);
 	}
 
-	//TODO replace with socket/server code
+//--- Socket --- //
+	int server_fd, new_socket, valread;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buffer[128] = {0};
+
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	// Forcefully attaching socket to the port 8080
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
+
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 10) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	int64 startTime = time(NULL);
+
 	while (running) {
+		mUSV::RequestData requestData;
+		mUSV::SensorData sensorData;
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
+		{
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+		valread = read(new_socket, buffer, 128);
+		if (!requestData.ParseFromString(buffer)) {
+			cerr << "Failed to parse Request Data message." << endl;
+			return -1;
+		}
+
+		// TODO find correct robot using requestData.tag_id()
 		pose2D pose = robots[0].getPose();
-		cout << "x:" << pose.x << " y:" << pose.y << " yaw:" << pose.yaw << endl;
+//		cout << "x:" << pose.x << " y:" << pose.y << " yaw:" << pose.yaw << endl;
+
+		sensorData.set_pose_x(pose.x);
+		sensorData.set_pose_y(pose.y);
+		sensorData.set_pose_yaw(pose.yaw);
+		sensorData.add_obstacle_sensors(0);
+		sensorData.add_obstacle_sensors(0);
+		sensorData.add_obstacle_sensors(0);
+		sensorData.add_puck_sensors(0);
+		sensorData.add_puck_sensors(0);
+		sensorData.add_puck_sensors(0);
+		*sensorData.mutable_last_updated() = TimeUtil::SecondsToTimestamp(time(NULL) - startTime);
+
+		size_t size = sensorData.ByteSizeLong();
+		char* msg = new char [size];
+		sensorData.SerializeToArray(msg, size);
+
+		send(new_socket, msg, size, 0);
+		close(new_socket);
+		memset(buffer,0,128);
+		delete[] msg;
 	}
 
 	void* status;
