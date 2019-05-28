2506cbc51b7fd1d60ab22db156e98ce5eea3ceb5
CalvinGregory <cgregory@mun.ca>
2019-05-28 17:11:35 -0230

    Replaced pthread with std::thread. Made socket non-blocking.

--- CVSensorSimulator/src/CVSensorSimulator.cpp
+++ CVSensorSimulator/src/CVSensorSimulator.cpp
@@ -1,15 +1,18 @@
 //============================================================================
 // Name        : CVSensorSimulator.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : CalvinGregory
+// Version     : 1.0
+// Copyright   : CERN Open Hardware Licence v1.2
+// Description : Computer-Vision based sensor simulator for the microUSV
 //============================================================================
 
 #include <iostream>
 #include <unistd.h>
 #include <netinet/in.h>
-#include "pthread.h"
+#include <thread>
+#include <functional>
+#include <fcntl.h>
+#include <errno.h>
 #include "opencv2/opencv.hpp"
 #include <google/protobuf/util/time_util.h>
 
@@ -29,30 +32,27 @@ using namespace std;
 using namespace cv;
 using google::protobuf::util::TimeUtil;
 
-#define PORT 8080
+#define PORT 8078
 
 bool running;
 int visualize = 1;
 
-void* vid_cap_thread(void* args) {
-	FrameBuffer* fb = (FrameBuffer*)args;
+void vid_cap_thread(FrameBuffer& fb) {
 	while(running) {
-		fb->updateFrame();
+		fb.updateFrame();
 	}
-	return NULL;
 }
 
-void* detector_thread(void* args) {
-	PoseDetector* pd = (PoseDetector*)args;
+void detector_thread(PoseDetector& pd) {
 
 	Mat temp_frame(100, 100, CV_8UC3, Scalar(0,0,0));
 	Mat* frame = &temp_frame;
 	imshow("RobotDetections", *frame);
 
 	while (running) {
-		pd->updatePoseEstimates();
+		pd.updatePoseEstimates();
 		if(visualize) {
-			frame = pd->getLabelledFrame();
+			frame = pd.getLabelledFrame();
 			imshow("RobotDetections", *frame);
 		}
 		// ESC key to exit
@@ -60,7 +60,6 @@ void* detector_thread(void* args) {
 			running = false;
 		}
 	}
-	return NULL;
 }
 
 int main(int argc, char* argv[]) {
@@ -100,24 +99,18 @@ int main(int argc, char* argv[]) {
 	}
 
 	FrameBuffer fb(settings);
-
 	PoseDetector pd(&fb, info, &robots, size);
 
-	pthread_t threads [2];
+	thread threads[2];
+	threads[0] = thread(vid_cap_thread, ref(fb));
+	threads[1] = thread(detector_thread, ref(pd));
 
-	int rc = pthread_create(&threads[0], NULL, vid_cap_thread, &fb);
-	if (rc) {
-		cout << "Error:unable to create video capture thread," << rc << endl;
-		exit(-1);
-	}
-
-	rc = pthread_create(&threads[1], NULL, detector_thread, &pd);
-	if (rc) {
-		cout << "Error:unable to create detector thread," << rc << endl;
-		exit(-1);
-	}
+	threads[0].detach();
+	threads[1].detach();
 
 //--- Socket --- //
+	//TODO encapsulate socket code
+	//TODO wishlist: add threadpool & multiple ports to handle client requests in parallel
 	int server_fd, new_socket, valread;
 	struct sockaddr_in address;
 	int opt = 1;
@@ -131,17 +124,20 @@ int main(int argc, char* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	// Forcefully attaching socket to the port 8080
+	// Forcefully attaching socket to the port
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
 	{
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
+	// Make socket non-blocking
+	fcntl(server_fd, F_SETFL, O_NONBLOCK);
+
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons( PORT );
 
-	// Forcefully attaching socket to the port 8080
+	// Forcefully attaching socket to the port
 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
 	{
 		perror("bind failed");
@@ -158,45 +154,46 @@ int main(int argc, char* argv[]) {
 	while (running) {
 		mUSV::RequestData requestData;
 		mUSV::SensorData sensorData;
-		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
-		{
-			perror("accept");
-			exit(EXIT_FAILURE);
+
+		new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+		if(new_socket == -1) {
+			if (errno != EWOULDBLOCK) {
+				perror("error accepting connection");
+				exit(EXIT_FAILURE);
+			}
+
 		}
-		valread = read(new_socket, buffer, 128);
-		if (!requestData.ParseFromString(buffer)) {
-			cerr << "Failed to parse Request Data message." << endl;
-			return -1;
+		else {
+			valread = read(new_socket, buffer, 128);
+			if (!requestData.ParseFromString(buffer)) {
+				cerr << "Failed to parse Request Data message." << endl;
+				return -1;
+			}
+
+			//TODO find correct robot using requestData.tag_id()
+			pose2D pose = robots[0].getPose();
+//			cout << "x:" << pose.x << " y:" << pose.y << " yaw:" << pose.yaw << endl;
+
+			sensorData.set_pose_x(pose.x);
+			sensorData.set_pose_y(pose.y);
+			sensorData.set_pose_yaw(pose.yaw);
+			sensorData.add_obstacle_sensors(0);
+			sensorData.add_obstacle_sensors(0);
+			sensorData.add_obstacle_sensors(0);
+			sensorData.add_puck_sensors(0);
+			sensorData.add_puck_sensors(0);
+			sensorData.add_puck_sensors(0);
+			*sensorData.mutable_last_updated() = TimeUtil::SecondsToTimestamp(time(NULL) - startTime);
+
+			size_t size = sensorData.ByteSizeLong();
+			char* msg = new char [size];
+			sensorData.SerializeToArray(msg, size);
+
+			send(new_socket, msg, size, 0);
+			close(new_socket);
+			memset(buffer,0,128);
+			delete[] msg;
 		}
-
-		// TODO find correct robot using requestData.tag_id()
-		pose2D pose = robots[0].getPose();
-//		cout << "x:" << pose.x << " y:" << pose.y << " yaw:" << pose.yaw << endl;
-
-		sensorData.set_pose_x(pose.x);
-		sensorData.set_pose_y(pose.y);
-		sensorData.set_pose_yaw(pose.yaw);
-		sensorData.add_obstacle_sensors(0);
-		sensorData.add_obstacle_sensors(0);
-		sensorData.add_obstacle_sensors(0);
-		sensorData.add_puck_sensors(0);
-		sensorData.add_puck_sensors(0);
-		sensorData.add_puck_sensors(0);
-		*sensorData.mutable_last_updated() = TimeUtil::SecondsToTimestamp(time(NULL) - startTime);
-
-		size_t size = sensorData.ByteSizeLong();
-		char* msg = new char [size];
-		sensorData.SerializeToArray(msg, size);
-
-		send(new_socket, msg, size, 0);
-		close(new_socket);
-		memset(buffer,0,128);
-		delete[] msg;
-	}
-
-	void* status;
-	for (int i = 0; i < 2; i++) {
-		pthread_join (threads[i], &status);
 	}
 
 	destroyAllWindows();
