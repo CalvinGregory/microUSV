aab8f33c4f1537f2043f394f212c40307fbb6761
CalvinGregory <cgregory@mun.ca>
2019-10-04 16:13:53 -0230

    Added example_config.json files to CVSensorSimulator and MUSVController. Update .gitignore to ignore json files. Update webcam_settings script for ubuntu 18.04.
    example_config.json files will be shown to users. all other json will be hidden and unique to that local copy to allow different config.json files on each microUSV.

--- CVSensorSimulator/webcam_settings.sh
+++ CVSensorSimulator/webcam_settings.sh
@@ -1,17 +1,17 @@
 # Script to configure webcam settings for use with the microUSV.
 
 # Disable Auto focus
-v4l2-ctl -d /dev/video1 --set-ctrl=focus_auto=0
-v4l2-ctl -d /dev/video1 --set-ctrl=focus_absolute=0
+v4l2-ctl -d /dev/video2 --set-ctrl=focus_auto=0
+v4l2-ctl -d /dev/video2 --set-ctrl=focus_absolute=0
 
 # Set exposure manually
-v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto=1
-v4l2-ctl -d /dev/video1 --set-ctrl=exposure_absolute=250
+v4l2-ctl -d /dev/video2 --set-ctrl=exposure_auto=1
+v4l2-ctl -d /dev/video2 --set-ctrl=exposure_absolute=250
 
 # Set gain level
-v4l2-ctl -d /dev/video1 --set-ctrl=gain=255
+v4l2-ctl -d /dev/video2 --set-ctrl=gain=255
 
 # Set sharpness
-v4l2-ctl -d /dev/video1 --set-ctrl=sharpness=255
+v4l2-ctl -d /dev/video2 --set-ctrl=sharpness=255
 
 
